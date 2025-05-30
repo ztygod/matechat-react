e2ff2d3cb9145a29dcec57d7a05fd4ce3e9b4d73
苏向夜 <fu050409@163.com>
2025-05-30 23:03:27 +0800

    chore(tsdown): bump tsdown to fix dts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,19 +1,6 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig([
-  // Rolldown will generate renamed exports in `d.ts` files
-  // Tracking: https://github.com/rolldown/rolldown/issues/4698
-  // Tracking: https://github.com/rolldown/tsdown/issues/269
-  // This is a workaround to avoid generating renamed exports.
-  {
-    entry: ["./src/index.ts"],
-    platform: "neutral",
-    dts: {
-      emitDtsOnly: true,
-    },
-    unbundle: true,
-    external: ["react", "react-dom", "react/jsx-runtime"],
-  },
   {
     entry: ["./src/index.ts"],
     platform: "neutral",
