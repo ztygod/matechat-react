a0b5fc8d720460ca7c6df562fbcbe3ada26805b3
TMZZ-03 <142074016+TMZZ031130@users.noreply.github.com>
2025-06-22 17:12:30 +0800

    feat(list): add List component (#26)
    Resolved https://github.com/DevCloudFE/matechat-react/issues/6
    
    ---------
    
    Co-authored-by: 苏向夜 <46275354+fu050409@users.noreply.github.com>

---
+++ .changes/add-list-component.md
@@ -0,0 +1,5 @@
+---
+"@matechat/react": patch:feat
+---
+
+Add `List` component in `list.tsx` to support grouped or normal lists.
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./bubble";
 export * from "./button";
+export * from "./list";
 export * from "./prompt";
 export * from "./sender";
