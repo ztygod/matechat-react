0dc43cc7bc3566632a80f413ab1d1c0ede7cd688
苏向夜 <fu050409@163.com>
2025-07-08 11:44:54 +0800

    chore(watch): optimize watch script

--- scripts/watch.ts
+++ scripts/watch.ts
@@ -2,10 +2,49 @@ import { execSync, spawn } from "node:child_process";
 import fs, { type WatchEventType } from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import { createSpinner, type ProgressBar } from "archons";
+import { createSpinner, defineCommand, type ProgressBar, run } from "archons";
 import chalk from "chalk";
 
-function isIgnored(filename: string) {
+export interface WatchConfig {
+  paths: { path: string; cmd: string }[];
+  dev: {
+    exec: string;
+    options: string[];
+  };
+}
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const watchMap: Record<string, WatchConfig> = {
+  core: {
+    paths: [{ path: path.join(__dirname, "../src"), cmd: "pnpm build" }],
+    dev: {
+      exec: "pnpm",
+      options: ["run", "--silent", "preview", "--clearScreen", "false"],
+    },
+  },
+  docs: {
+    paths: [{ path: path.join(__dirname, "../src"), cmd: "pnpm build" }],
+    dev: {
+      exec: "pnpm",
+      options: ["run", "--silent", "--filter", "@matechat/react-docs", "dev"],
+    },
+  },
+};
+const ignored = [
+  "node_modules",
+  ".git",
+  ".github",
+  ".vscode",
+  "dist",
+  "build",
+  "bin",
+  ".md",
+  ".d.ts",
+  "target",
+  ".cjs",
+  "doc_build",
+];
+
+function isIgnored(filename: string, ignored: string[]) {
   for (const pattern of ignored) {
     if (filename.includes(pattern)) {
       return true;
@@ -31,68 +70,36 @@ function createArchonsSpinner() {
   return spinner;
 }
 
-const cwd = process.cwd();
-const watchMap = new Map<string, string>([
-  [
-    path.join(path.dirname(fileURLToPath(import.meta.url)), "../src"),
-    "pnpm build",
-  ],
-]);
-const displayPaths = Array.from(watchMap.keys())
-  .map((dirPath) => chalk.cyan(path.relative(cwd, dirPath)))
-  .join(", ");
-
-const ignored = [
-  "node_modules",
-  ".git",
-  ".github",
-  ".vscode",
-  "dist",
-  "build",
-  "bin",
-  ".md",
-  ".d.ts",
-  "target",
-  ".cjs",
-];
-const greenPrefix = chalk.green("[MateChat]");
-
-const spinner = createArchonsSpinner();
-spinner.enableSteadyTick(100);
-spinner.setMessage("Initial building...");
-for (const cmd of watchMap.values()) {
-  tryBuild(cmd, "Building...", spinner);
+export interface WatchOptions {
+  coloredPrefix: string;
+  options: WatchConfig;
+  displayPaths: string;
+  ignoredPaths: string[];
 }
-spinner.finishAndClear();
-console.clear();
-console.log(`${greenPrefix} Build complete.\n`);
-console.log(`${greenPrefix} Watching on ${displayPaths} for changes...`);
+function watch({
+  coloredPrefix,
+  options,
+  displayPaths,
+  ignoredPaths,
+}: WatchOptions) {
+  let lastBuild = Date.now();
 
-function watch() {
   function createDevProcess() {
-    let devProcess = spawn(
-      "pnpm",
-      ["run", "--silent", "preview", "--clearScreen", "false"],
-      {
-        stdio: "inherit",
-        shell: true,
-      },
-    );
+    let devProcess = spawn(options.dev.exec, options.dev.options, {
+      stdio: "inherit",
+      shell: true,
+    });
     return () => {
       const spinner = createArchonsSpinner();
-      spinner.println(`${greenPrefix} Restarting Vite dev server...`);
+      spinner.println(`${coloredPrefix} Restarting dev server...`);
       while (!devProcess.kill("SIGINT"));
       devProcess.on("close", () => {
-        devProcess = spawn(
-          "pnpm",
-          ["run", "--silent", "preview", "--clearScreen", "false"],
-          {
-            stdio: "inherit",
-            shell: true,
-          },
-        );
+        devProcess = spawn(options.dev.exec, options.dev.options, {
+          stdio: "inherit",
+          shell: true,
+        });
         devProcess.on("error", (err) => {
-          console.error(`${greenPrefix} Error restarting dev process:`, err);
+          console.error(`${coloredPrefix} Error restarting dev process:`, err);
         });
         spinner.finishAndClear();
       });
@@ -111,29 +118,77 @@ function watch() {
 
     restartServer();
 
-    if (filename && !isIgnored(filename)) {
+    if (filename && !isIgnored(filename, ignoredPaths)) {
       const spinner = createArchonsSpinner();
       spinner.println(
-        `${greenPrefix} File ${chalk.cyan(filename)} was ${eventType}d, rebuilding...`,
+        `${coloredPrefix} File ${chalk.cyan(
+          filename,
+        )} was ${eventType}d, rebuilding...`,
       );
       spinner.enableSteadyTick(100);
       if ([".ts", ".tsx", ".css"].some((ext) => filename.endsWith(ext))) {
         tryBuild(command, "Rebuilding...", spinner);
       }
       console.clear();
-      spinner.println(`${greenPrefix} Build complete.\n\n`);
+      spinner.println(`${coloredPrefix} Build complete.\n\n`);
       spinner.println(
-        `${greenPrefix} Watching on ${displayPaths} for changes...`,
+        `${coloredPrefix} Watching on ${displayPaths} for changes...`,
       );
       spinner.finishAndClear();
       lastBuild = Date.now();
     }
   }
 
-  for (const [dirPath, cmd] of watchMap) {
-    fs.watch(dirPath, { recursive: true }, rebuild.bind(null, cmd));
+  for (const { cmd, path } of options.paths) {
+    fs.watch(path, { recursive: true }, rebuild.bind(null, cmd));
   }
 }
 
-let lastBuild = Date.now();
-watch();
+const main = defineCommand({
+  meta: {
+    name: "watch",
+    about: "Watch files and rebuild when changed",
+    styled: true,
+  },
+  options: {
+    project: {
+      type: "option",
+      default: "core",
+      short: "p",
+      help: "Project to watch",
+    },
+  },
+  callback: async (ctx) => {
+    const cwd = process.cwd();
+    const project = ctx.args.project as string;
+    if (!watchMap[project]) {
+      throw new Error(`Project ${project} not found`);
+    }
+
+    const displayPaths = watchMap[project].paths
+      .map((opts) => chalk.cyan(path.relative(cwd, opts.path)))
+      .join(", ");
+
+    const coloredPrefix = chalk.green("[MateChat]");
+
+    const spinner = createArchonsSpinner();
+    spinner.enableSteadyTick(100);
+    spinner.setMessage("Initial building...");
+    for (const { cmd } of watchMap[project].paths) {
+      tryBuild(cmd, "Building...", spinner);
+    }
+    spinner.finishAndClear();
+    console.clear();
+    console.log(`${coloredPrefix} Build complete.\n`);
+    console.log(`${coloredPrefix} Watching on ${displayPaths} for changes...`);
+
+    watch({
+      coloredPrefix,
+      options: watchMap[project],
+      displayPaths,
+      ignoredPaths: ignored,
+    });
+  },
+});
+
+run(main);
